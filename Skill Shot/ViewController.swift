ba96f9333ec5321e88a223683f8e10ede52f7c2c
Will Clarke <me@willclarke.net>
2015-12-21 18:11:12 -0800

    Initial Commit

---
+++ Skill Shot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skill Shot
+//
+//  Created by Will Clarke on 12/21/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
