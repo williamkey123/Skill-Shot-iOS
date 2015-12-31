338aaf04f202c519b73171f1511bdc967bdbf964
Will Clarke <me@willclarke.net>
2015-12-30 18:12:58 -0800

    Added required UISearchResultsUpdating protocol function

--- Skill Shot/Controllers/LocationTableViewController.swift
+++ Skill Shot/Controllers/LocationTableViewController.swift
@@ -147,5 +147,11 @@ class LocationTableViewController: UITableViewController, UISearchResultsUpdatin
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: UISearchResultsUpdating functions
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        return
+    }
 
 }
